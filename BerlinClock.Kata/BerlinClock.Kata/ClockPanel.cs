41a8042798465a8c694a173692e48100308d1a42
Boris Sclauzero <riversx@users.noreply.github.com>
2015-03-04 00:41:35 +0100

    Initial Commit

---
+++ BerlinClock.Kata/BerlinClock.Kata/ClockPanel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BerlinClock
+{
+
+    public class ClockPanel
+    {
+
+        public string Row1 { get; private set; }
+
+        public object Row2 { get; private set; }
+
+        public string Row3 { get; private set; }
+
+        public string Row4 { get; private set; }
+
+        public string Row5 { get; private set; }
+
+        private ClockPanel() { }
+
+        public ClockPanel(TimeSpan time)
+        {
+            SetRow1(time.Seconds);
+            SetRow2(time.Hours / 5);
+            SetRow3(time.Hours % 5);
+            SetRow4(time.Minutes / 5);
+            SetRow5(time.Minutes % 5);
+        }
+
+        private void SetRow1(int seconds)
+        {
+            Row1 = (seconds % 2 == 0) ? "Y" : "O";
+        }
+
+        private void SetRow2(int fiveHours)
+        {
+            Row2 = "RRRR".Substring(0, fiveHours) + "OOOO".Substring(fiveHours);
+        }
+
+        private void SetRow3(int lastFiveHours)
+        {
+            Row3 = "RRRR".Substring(0, lastFiveHours) + "OOOO".Substring(lastFiveHours);
+        }
+
+        private void SetRow4(int fiveMinutes)
+        {
+            Row4 = "YYRYYRYYRYY".Substring(0,fiveMinutes) + "OOOOOOOOOOO".Substring(fiveMinutes);
+        }
+
+        private void SetRow5(int lastFiveMinutes)
+        {
+            Row5 = "YYYY".Substring(0, lastFiveMinutes) + "OOOO".Substring(lastFiveMinutes);
+        }
+
+        public override string ToString()
+        {
+            return Row1 + "\n" + Row2 + "\n" + Row3 + "\n" + Row4 + "\n" + Row5;
+        }
+
+    }
+
+}
---
+++ BerlinClock.Kata/BerlinClock.Kata/ClockPanelTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using BerlinClock;
+
+namespace BerlinClock.UnitTests
+{
+    [TestFixture]
+    public class ClockPanelTests
+    {
+
+        [TestCase(0, 0, 0, "Y")]
+        [TestCase(0, 0, 1, "O")]
+        [TestCase(0, 0, 2, "Y")]
+        public void ClockPanelConstructor_InitWithSeconds_ShowsCorretlyRow1(int hours, int minutes, int seconds, string expected)
+        {
+            ClockPanel clock = new ClockPanel(new TimeSpan(hours, minutes, seconds));
+
+            Assert.AreEqual(expected, clock.Row1);
+        }
+
+        [TestCase(0, 0, 0, "OOOO")]
+        [TestCase(0, 1, 0, "YOOO")]
+        [TestCase(0, 2, 0, "YYOO")]
+        [TestCase(0, 5, 0, "OOOO")]
+        public void ClockPanelConstructor_InitWithMinutes_ShowsCorretlyRow5(int hours, int minutes, int seconds, string expected)
+        {
+            ClockPanel clock = new ClockPanel(new TimeSpan(hours, minutes, seconds));
+
+            Assert.AreEqual(expected, clock.Row5);
+        }
+
+        [TestCase(0, 0, 0, "OOOOOOOOOOO")]
+        [TestCase(0, 5, 0, "YOOOOOOOOOO")]
+        [TestCase(0, 6, 0, "YOOOOOOOOOO")]
+        [TestCase(0, 10, 0, "YYOOOOOOOOO")]
+        [TestCase(0, 15, 0, "YYROOOOOOOO")]
+        public void ClockPanelConstructor_InitWithMinutes_ShowsCorretlyRow4(int hours, int minutes, int seconds, string expected)
+        {
+            ClockPanel clock = new ClockPanel(new TimeSpan(hours, minutes, seconds));
+
+            Assert.AreEqual(expected, clock.Row4);
+        }
+
+        [TestCase(0, 0, 0, "OOOO")]
+        [TestCase(1, 0, 0, "ROOO")]
+        [TestCase(2, 0, 0, "RROO")]
+        [TestCase(5, 0, 0, "OOOO")]
+        public void ClockPanelConstructor_InitWithHours_ShowsCorrettlyRow3(int hours, int minutes, int seconds, string expected)
+        {
+            ClockPanel clock = new ClockPanel(new TimeSpan(hours, minutes, seconds));
+
+            Assert.AreEqual(expected, clock.Row3);
+        }
+
+        [TestCase(0, 0, 0, "OOOO")]
+        [TestCase(5, 0, 0, "ROOO")]
+        [TestCase(15, 0, 0, "RRRO")]
+        [TestCase(23, 0, 0, "RRRR")]
+        public void ClockPanelConstructor_InitWithHours_ShowsCorrectlyRow2(int hours, int minutes, int seconds, string expected)
+        {
+            ClockPanel clock = new ClockPanel(new TimeSpan(hours, minutes, seconds));
+
+            Assert.AreEqual(expected, clock.Row2);
+        }
+
+        [TestCase(0, 0, 0, "Y\nOOOO\nOOOO\nOOOOOOOOOOO\nOOOO")]
+        [TestCase(16, 50, 6, "Y\nRRRO\nROOO\nYYRYYRYYRYO\nOOOO")]
+        [TestCase(23, 59, 59, "O\nRRRR\nRRRO\nYYRYYRYYRYY\nYYYY")]
+        [TestCase(11, 37, 01, "O\nRROO\nROOO\nYYRYYRYOOOO\nYYOO")]
+        public void ClockPanelConstructor_InitWithSpecicTime_ShowsCorrectyAllRows(int hours, int minutes, int seconds, string expected)
+        {
+            ClockPanel clock = new ClockPanel(new TimeSpan(hours, minutes, seconds));
+
+            Assert.AreEqual(expected, clock.ToString());
+        }
+
+    }
+}
